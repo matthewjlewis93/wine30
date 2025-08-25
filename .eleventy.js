dd3793147332e99f4635ac34ec1197df77a489ec
matthewjlewis93 <matthewjlewis93@gmail.com>
2025-08-25 18:34:54 -0500

    edit build command

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
 module.exports = (config) => {
+  config.addPassthroughCopy("./src/index.css/");
+  config.addPassthroughCopy("./src/images/");
+
+  config.addCollection("storehours", collection => {
+    return collection.getFilteredByGlob("./src/hours/*.md").sort((a,b) => a.data.order > b.data.order ? 1 : -1 )
+  })
+
   return {
     markdownTemplateEngine: "njk",
     dataTemplateEngine: "njk",
