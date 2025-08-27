3d9c1fd4c845c900b15e9d1d888fe8fd9e3037ba
matthewjlewis93 <matthewjlewis93@gmail.com>
2025-08-27 16:51:23 -0500

    fixed current day calculation

---
+++ public/index.js
@@ -0,0 +1,7 @@
+[...document.getElementsByClassName("schedule-day")].forEach((day, index) => {
+  if (
+    new Date().getDay() === [1,1,2,2,3,3,4,4,5,5,6,6,0,0][index]
+  ) {
+    day.classList.add("current-day");
+  }
+});
--- src/_data/hoursFunctions.js
+++ src/_data/hoursFunctions.js
@@ -5,23 +5,6 @@ module.exports = {
     }
     return "Closed";
   },
-  getCurrentDay(day) {
-    if (
-      day.title ===
-      [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ][new Date().getDay()]
-    ) {
-      return "current-day";
-    }
-    return "";
-  },
   sortDays(hoursObj) {
     let dayArr = Object.values(hoursObj);
     dayArr.sort((a,b) => a.order - b.order)
