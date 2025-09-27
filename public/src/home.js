7c367cffcaf509298a7869e98336e8e1e4cc5f09
matthewjlewis93 <matthewjlewis93@gmail.com>
2025-09-27 00:43:39 -0500

    site rebuild

--- public/src/home.js
+++
@@ -1,7 +0,0 @@
-[...document.getElementsByClassName("schedule-day")].forEach((day, index) => {
-  if (
-    new Date().getDay() === [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 0, 0][index]
-  ) {
-    day.classList.add("current-day");
-  }
-});
