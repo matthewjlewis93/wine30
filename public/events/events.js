cf4479561f32e20be75a1421fad18f345787192e
matthewjlewis93 <matthewjlewis93@gmail.com>
2025-11-12 23:21:08 -0600

    calendar event tag

--- public/events/events.js
+++ public/events/events.js
@@ -31,17 +31,35 @@ createCalendar = (dateObject) => {
   document.getElementById("calendar-body").innerHTML = "";
   const calendarArray = [];
   for (i = 1 - dateObject["Day of 1"]; i <= dateObject["Days in Month"]; i++) {
+    let dateString = `${dateObject["Current Month"].getFullYear()}-${
+      dateObject["Current Month"].getMonth() + 1
+    }-${String(i).padStart(3, "0")}`;
+    const dateMatch = document.querySelector(`[date='${dateString}']`);
+    let formattedDate;
+    if (dateMatch) {
+      formattedDate = new Date(dateMatch.children[2].innerText);
+      formattedTime = formattedDate.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: "true"
+      })
+    }
+
     if (i <= 0) {
       calendarArray.push("<div class='calendar-day' ></div");
     } else {
       calendarArray.push(
-        `<div class='calendar-day'><p class=${
+        `<div class='calendar-day'><p date=${dateString} class=${
           monthOffset === 0
             ? new Date().getDate() === i
               ? " todays-date"
               : ""
             : ""
-        }>${i}</p></div>`
+        }>${i}</p>${
+          dateMatch
+            ? `<p style='font-size: 0.6rem; text-align: center; position: absolute; inset: 0px; width: 100%; height: 100%;' ><span style=' background-color: white; border-top-left-radius: 2px; border-bottom-left-radius: 2px; padding: 2px' >❗</span><span style='background-color: #eee; border-top-right-radius: 2px; border-bottom-right-radius: 2px; padding: 2px'' > ${formattedTime}</span></p>`
+            : ""
+        }</div>`
       );
     }
   }
