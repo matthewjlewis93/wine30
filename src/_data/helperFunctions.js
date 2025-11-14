6b9f7610308b71fed0c1992b1c9e1e779d514ad8
matthewjlewis93 <matthewjlewis93@gmail.com>
2025-11-14 16:04:59 -0600

    netlify date rework

--- src/_data/helperFunctions.js
+++ src/_data/helperFunctions.js
@@ -7,18 +7,24 @@ module.exports = {
   },
   sortDays(hoursObj) {
     let dayArr = Object.values(hoursObj);
-    dayArr.sort((a,b) => a.order - b.order)
+    dayArr.sort((a, b) => a.order - b.order);
     return dayArr;
   },
-  dateProcessor (objWithDate) {
-    let date = new Date(objWithDate.date)
-    date = new Date(`${date.getFullYear()}-${
-      date.getMonth() + 1
-    }-${String(date.getDate()).padStart(3, "0")}`);
+  dateProcessor(objWithDate) {
+    // let date = new Date(objWithDate.date)
+    // date = new Date(`${date.getFullYear()}-${
+    //   date.getMonth() + 1
+    // }-${String(date.getDate()).padStart(3, "0")}`);
 
-    processedObj = {...objWithDate, month: date.getMonth() +1, day: date.getDate(), year: date.getFullYear() }
+    let processedObj = {
+      ...objWithDate,
+      month: objWithDate.date.slice(5, 7),
+      day: objWithDate.date.slice(8, 10),
+      year: objWithDate.date.slice(0, 4),
+    };
 
-    return processedObj
-  }
+    // processedObj = {...objWithDate, month: date.getMonth() +1, day: date.getDate(), year: date.getFullYear() }
 
+    return processedObj;
+  },
 };
